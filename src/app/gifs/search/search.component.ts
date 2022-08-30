134ed2bdb7b86ff44fbbe6ce18b745e890b1163a
sant803257 <sant803257@siigo.com>
2022-08-29 19:59:59 -0500

    02-GIFS: Creación de servicio para gifs.
    Servicio para almacenar listado de busquedas.

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-search',
@@ -8,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('inputSearch') inputSearch!: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private gifsService: GifsService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  searchGif() {
+
+    const value = this.inputSearch.nativeElement.value;
+    this. gifsService.searchGifs(value);
+    this.inputSearch.nativeElement.value = '';
+  }
 }
